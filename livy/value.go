ab61666afffad8654d03d28eeaa78ab95e8d5dd9
strick <strick@yak.net>
2018-08-24 00:01:51 -0400

    Pretty() & Shape() for interface Val

--- livy/value.go
+++ livy/value.go
@@ -8,7 +8,9 @@ import (
 
 type Val interface {
 	String() string
+	Pretty() string
 	Size() int
+	Shape() []int
 	Ravel() []Val
 	GetScalarInt() int
 	GetScalarFloat() float64
@@ -52,6 +54,46 @@ func (o Mat) String() string {
 	return bb.String()
 }
 func (o Box) String() string {
+	return fmt.Sprintf("<Box> ")
+}
+
+func (o Char) Pretty() string {
+	return fmt.Sprintf("'%c' ", o.R)
+}
+func (o Num) Pretty() string {
+	return fmt.Sprintf("%g ", o.F)
+}
+func (o Mat) Pretty() string {
+	var bb bytes.Buffer
+	rank := len(o.S)
+	switch rank {
+	case 0:
+		return "(* TODO: Mat rank 0 *) " /* + o.M[0].Pretty() */
+	case 1:
+		if len(o.M) != o.S[0] {
+			log.Panicf("matrix shape %v but contains %d elements: %#v", o.S, len(o.M), o)
+		}
+		for _, v := range o.M {
+			bb.WriteString(v.String())
+		}
+	case 2:
+		for i := 0; i < o.S[0]; i++ {
+			begin := i * o.S[1]
+			end := (i + 1) * o.S[1]
+			bb.WriteString(Mat{M: o.M[begin:end], S: o.S[1:]}.Pretty())
+			bb.WriteString("\n")
+		}
+	default:
+		for i := 0; i < o.S[0]; i++ {
+			begin := i * mulReduce(o.S[1:])
+			end := (i + 1) * mulReduce(o.S[1:])
+			bb.WriteString(Mat{M: o.M[begin:end], S: o.S[1:]}.Pretty())
+			bb.WriteString("\n")
+		}
+	}
+	return bb.String()
+}
+func (o Box) Pretty() string {
 	return fmt.Sprintf("<Box> ")
 }
 
@@ -124,6 +166,19 @@ func (o Mat) Size() int {
 }
 func (o Box) Size() int {
 	return 1
+}
+
+func (o Char) Shape() []int {
+	return nil
+}
+func (o Num) Shape() []int {
+	return nil
+}
+func (o Mat) Shape() []int {
+	return o.S
+}
+func (o Box) Shape() []int {
+	return nil
 }
 
 func (o Char) Ravel() []Val {
